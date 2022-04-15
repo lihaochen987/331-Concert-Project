1d8739c4d6ff7876b03f0df9ed9736298ffef1fe
HaoChen <lihaochen987@gmail.com>
2022-04-15 16:47:02 +1200

    Initialised User class.

--- concert-service/src/main/java/proj/concert/service/domain/User.java
+++ concert-service/src/main/java/proj/concert/service/domain/User.java
@@ -1,4 +1,57 @@
 package proj.concert.service.domain;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class User {
+
+    private String username;
+    private String password;
+
+    protected User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User userDTO = (User) o;
+
+        return new EqualsBuilder()
+                .append(username, userDTO.username)
+                .append(password, userDTO.password)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(username)
+                .append(password)
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
